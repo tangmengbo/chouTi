326afd89e3b7dfdc64ccc1625be1aa60ebf04132
唐蒙波123 <251974230@qq.com>
2019-02-14 10:06:43 +0800

    first commit

---
+++ CWLateralSlideDemo/Clasess/Controllers/LeftViewController.m
@@ -0,0 +1,118 @@
+//
+//  LeftViewController.m
+//  ViewControllerTransition
+//
+//  Created by 陈旺 on 2017/7/10.
+//  Copyright © 2017年 chavez. All rights reserved.
+//
+
+#import "LeftViewController.h"
+#import "NextViewController.h"
+#import "CWTableViewInfo.h"
+#import "UIViewController+CWLateralSlide.h"
+
+@interface LeftViewController ()
+
+@property (nonatomic,weak) UITableView *tableView;
+
+@property (nonatomic,strong) NSArray *imageArray;
+@property (nonatomic,strong) NSArray *titleArray;
+
+@end
+
+@implementation LeftViewController
+{
+    CWTableViewInfo *_tableViewInfo;
+}
+
+- (void)dealloc {
+    NSLog(@"%s",__func__);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupHeader];
+    [self setupTableView];
+    
+}
+
+- (void)setupHeader {
+    UIImageView *imageV = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kCWSCREENWIDTH * 0.75, 200)];
+    imageV.backgroundColor = [UIColor clearColor];
+    imageV.contentMode = UIViewContentModeScaleAspectFill;
+    imageV.image = [UIImage imageNamed:@"1.jpg"];
+    [self.view addSubview:imageV];
+}
+
+- (void)setupTableView {
+    
+    _tableViewInfo = [[CWTableViewInfo alloc] initWithFrame:CGRectMake(0, 300, kCWSCREENWIDTH * 0.75, CGRectGetHeight(self.view.bounds)-300) style:UITableViewStylePlain];
+    
+    for (int i = 0; i < self.titleArray.count; i++) {
+        NSString *title = self.titleArray[i];
+        NSString *imageName = self.imageArray[i];
+        SEL sel = @selector(didSelectCell:indexPath:);
+        CWTableViewCellInfo *cellInfo = [CWTableViewCellInfo cellInfoWithTitle:title imageName:imageName target:self sel:sel];
+        [_tableViewInfo addCell:cellInfo];
+    }
+    
+    [self.view addSubview:[_tableViewInfo getTableView]];
+    [[_tableViewInfo getTableView] reloadData];
+}
+
+#pragma mark - cell点击事件
+- (void)didSelectCell:(CWTableViewCellInfo *)cellInfo indexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == self.titleArray.count - 1) { // 点击最后一个主动收起抽屉
+        [self dismissViewControllerAnimated:YES completion:nil];
+        return;
+    }
+    
+    if (indexPath.row == self.titleArray.count - 2) { // 显示alertView
+        [self showAlterView];
+        return;
+    }
+    
+    NextViewController *vc = [NextViewController new];
+    if (indexPath.row == 0) {
+        UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
+        [self cw_presentViewController:nav];
+    }else {
+        [self cw_pushViewController:vc];
+    }
+}
+
+- (void)showAlterView {
+    UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"hello world!" message:@"hello world!嘿嘿嘿" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"😂😄" style:UIAlertActionStyleDefault handler:nil];
+    [alertC addAction:action];
+    [self presentViewController:alertC animated:YES completion:nil];
+}
+
+#pragma mark - Getter
+- (NSArray *)imageArray {
+    if (_imageArray == nil) {
+        _imageArray = @[@"personal_member_icons",
+                        @"personal_myservice_icons",
+                        @"personal_news_icons",
+                        @"personal_order_icons",
+                        @"personal_preview_icons",
+                        @"personal_service_icons"];
+    }
+    return _imageArray;
+}
+
+- (NSArray *)titleArray{
+    if (_titleArray == nil) {
+        _titleArray = @[@"present下一个界面",
+                        @"Push下一个界面",
+                        @"Push下一个界面",
+                        @"Push下一个界面",
+                        @"显示alertView",
+                        @"主动收起抽屉"];
+    }
+    return _titleArray;
+}
+
+
+@end
